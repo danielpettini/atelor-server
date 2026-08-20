3526038eba3ac52c4eeb23a648c48136769afe9f
Daniel <danielpettini@exemplo.com>
2026-08-20 15:15:15 -0300

    Atualizando configuracoes do servidor

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post('/license/validate', async (req, res) => {
 
 // ********** INICIAR SERVIDOR **********
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
