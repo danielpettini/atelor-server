a9a0b2166a7d0ae58adecead1f6f49e8b9f1442a
Daniel <danielpettini@exemplo.com>
2026-08-20 10:00:40 -0300

    Libera CORS total

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const mongoose = require('mongoose');
 const License = require('./License');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // ********** CONEXÃO COM O BANCO **********
